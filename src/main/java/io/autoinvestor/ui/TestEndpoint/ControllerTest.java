f3d7fa55a4941999f24c5a50e96261eec60ea5d0
AlfredoMate <mate.alfredo98@gmail.com>
2025-05-05 19:51:50 +0200

    debug headers

--- src/main/java/io/autoinvestor/ui/TestEndpoint/ControllerTest.java
+++ src/main/java/io/autoinvestor/ui/TestEndpoint/ControllerTest.java
@@ -1,13 +1,18 @@
 package io.autoinvestor.ui.TestEndpoint;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class ControllerTest {
 
     @GetMapping("/api/testUsers")
-    public String handler() {
-        return "Congrats. You reached the users service.";
+    public String handler(@RequestHeader Map<String, String> headers) {
+        StringBuilder response = new StringBuilder("Received headers:\n");
+        headers.forEach((key, value) -> response.append(key).append(": ").append(value).append("\n"));
+        return response.toString();
     }
 }
