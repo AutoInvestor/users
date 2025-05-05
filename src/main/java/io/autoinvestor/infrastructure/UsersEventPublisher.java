488fc5ea1fa90d31182615732e91af6a49122bdc
AlfredoMate <mate.alfredo98@gmail.com>
2025-05-05 22:52:31 +0200

    template

---
+++ src/main/java/io/autoinvestor/infrastructure/UsersEventPublisher.java
@@ -0,0 +1,18 @@
+package io.autoinvestor.infrastructure;
+
+import com.google.cloud.spring.pubsub.core.PubSubTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersEventPublisher {
+
+    private static final String TOPIC = "users";
+    private final PubSubTemplate pubSubTemplate;
+
+    public UsersEventPublisher(PubSubTemplate pubSubTemplate) {
+        this.pubSubTemplate = pubSubTemplate;
+    }
+
+    //public void publishUserCreated(User)
+
+}
