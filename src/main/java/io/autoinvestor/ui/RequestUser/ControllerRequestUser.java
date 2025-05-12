e6c45baba8185679ba49d0981099f7b7e5808364
AlfredoMate <mate.alfredo98@gmail.com>
2025-05-12 13:17:12 +0200

    remove api from endpoints

--- src/main/java/io/autoinvestor/ui/RequestUser/ControllerRequestUser.java
+++ src/main/java/io/autoinvestor/ui/RequestUser/ControllerRequestUser.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/user")
 public class ControllerRequestUser {
 
     private GetUserQueryHandler getUserCommandHandler;
