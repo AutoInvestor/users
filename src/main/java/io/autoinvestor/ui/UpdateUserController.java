3e08947ee42cc1072027d747cd981c42cbed6254
AlfredoMate <mate.alfredo98@gmail.com>
2025-05-30 18:23:38 +0200

    smallFix5

--- src/main/java/io/autoinvestor/ui/UpdateUserController.java
+++ src/main/java/io/autoinvestor/ui/UpdateUserController.java
@@ -5,10 +5,7 @@ import io.autoinvestor.application.UpdateUserUseCase.UpdateUserCommandHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -16,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UpdateUserController {
 
     private final UpdateUserCommandHandler updateUserCommandHandler;
-
+    @PutMapping
     public ResponseEntity<Void> handle (
             @RequestHeader(value = "X-User-Id", required = false) String userId,
             @RequestBody UpdateUserDTO dto
